fdbc8025d140cf4393c4ac3fcfaab0983eaa077d
Matheus Brenner Lehmann <matios@gmail.com>
2024-05-01 10:50:14 +0200

    Refactor of problem 102

--- UVA/102 - Ecological Bin Packing.cpp
+++ UVA/102 - Ecological Bin Packing.cpp
@@ -1,61 +1,85 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
+struct Response
+{
+    int movements;
+    string bins;
 
- int lixos[3][3];
- int n[9];
-
- int sol[6];
- int max;
- int total;
- // Brown Green Clear
-
- while (cin >> n[0]) {
-  total = 0;
-  for (int i = 1; i < 9; i++) {
-   cin >> n[i];
-  }
-  for (int i = 0; i < 3; i++) {
-   for (int j = 0; j < 3; j++) {
-    lixos[i][j] = n[i*3+j];
-    total += n[i*3+j];
-   }
-  }
-
-  max = 0;
-
-  sol[0] = lixos[0][0] + lixos[1][2] + lixos[2][1];
-  sol[1] = lixos[0][0] + lixos[1][1] + lixos[2][2];
-  sol[2] = lixos[0][2] + lixos[1][0] + lixos[2][1];
-  sol[3] = lixos[0][2] + lixos[1][1] + lixos[2][0];
-  sol[4] = lixos[0][1] + lixos[1][0] + lixos[2][2];
-  sol[5] = lixos[0][1] + lixos[1][2] + lixos[2][0];
-
-  for (int i = 0; i < 6; i++) {
-   if (sol[i] > max) {
-    max = sol[i];
-   }
-  }
-
-  for (int i = 0; i < 6; i++) {
-   if (sol[i] == max) {
-    switch(i) {
-     case 0: cout << "BCG "; break;
-     case 1: cout << "BGC "; break;
-     case 2: cout << "CBG "; break;
-     case 3: cout << "CGB "; break;
-     case 4: cout << "GBC "; break;
-     case 5: cout << "GCB "; break;
+    Response(int movements, string bins) : movements(movements), bins(bins) {}
+    void print()
+    {
+        cout << bins << " " << movements << endl;
     }
-    cout << (total - max) << endl;
-    break;
-   }
-  }
+};
 
- }
+bool compareResponses(const Response& a, const Response& b)
+{
+    if (a.movements == b.movements) return a.bins < b.bins;
+    return a.movements < b.movements;
+}
 
- return 0;
+class RecyclingCenter
+{
+public:
+    RecyclingCenter()
+    {
+        bottles = 0;
+    }
 
+    void addBottlesToBin(int bin, int brown, int green, int clear)
+    {
+        bins[bin][0] = brown;
+        bins[bin][1] = green;
+        bins[bin][2] = clear;
+
+        bottles += (brown + green + clear);
+    }
+
+    Response getMinimumBottleMovements()
+    {
+        vector<Response> responses = vector<Response>();
+        responses.push_back(Response(getBottleMovements(0, 1, 2), "BGC"));
+        responses.push_back(Response(getBottleMovements(0, 2, 1), "BCG"));
+        responses.push_back(Response(getBottleMovements(1, 0, 2), "GBC"));
+        responses.push_back(Response(getBottleMovements(1, 2, 0), "CBG"));
+        responses.push_back(Response(getBottleMovements(2, 0, 1), "GCB"));
+        responses.push_back(Response(getBottleMovements(2, 1, 0), "CGB"));
+        sort(responses.begin(), responses.end(), compareResponses);
+        return responses.front();
+    }
+    
+private:
+    int bins[3][3];
+    int bottles;
+
+    int getBottleMovements(int brownBin, int greenBin, int clearBin)
+    {
+        return bottles - (bins[brownBin][0] + bins[greenBin][1] + bins[clearBin][2]);
+    }
+};
+
+int main()
+{
+    int brown;
+    int green;
+    int clear;
+
+    while (cin >> brown >> green >> clear)
+    {
+        RecyclingCenter recyclingCenter = RecyclingCenter();
+        recyclingCenter.addBottlesToBin(0, brown, green, clear);
+
+        cin >> brown >> green >> clear;
+        recyclingCenter.addBottlesToBin(1, brown, green, clear);
+
+        cin >> brown >> green >> clear;
+        recyclingCenter.addBottlesToBin(2, brown, green, clear);
+
+        Response response = recyclingCenter.getMinimumBottleMovements();
+        response.print();
+    }
 }
